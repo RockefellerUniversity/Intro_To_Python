5304f5166aa9bac2e12542d6651615dd1f135806
root <tc.infomatics@gmail.com>
2024-12-03 23:58:56 +0000

    Autobuild

--- docs/presentations/r_code/Session2.R
+++ docs/presentations/r_code/Session2.R
@@ -220,7 +220,7 @@ kable(minRepdf[,1:4], format='html')
 ## 
 
 ## 
-## sex, height, weight = np.genfromtxt('data/height-weight.csv', delimiter = ",", skip_header=True, unpack = True, dtype=None)
+## sex, height, weight = np.genfromtxt('data/height-weight.csv', delimiter = ",", skip_header=True, unpack = True, dtype=None, encoding=None)
 ## 
 ## sex
 
@@ -263,13 +263,14 @@ if(params$isSlides == "yes"){
 
 ## 
 ## import numpy as np
+## 
 ## # Read in dataset
-## sex, height, weight = np.genfromtxt('height-weight.csv', delimiter = ",", skip_header=True, unpack = True, dtype=None)
+## sex, height, weight = np.genfromtxt('data/height-weight.csv', delimiter = ",", skip_header=True, unpack = True, encoding=None, dtype=None)
 ## new_array = np.array([sex, height, weight])
 ## # Subset array
 ## to_subset = new_array[0,:]=="Female"
 ## subset_array = new_array[:,to_subset]
 ## # Print out number of Females in data set and export subset
 ## print(str(sum(to_subset)) + " are female")
-## np.savetxt("height-weight_female.csv", subset_array, delimiter=',', fmt='%s')
+## np.savetxt("data/height-weight_female.csv", subset_array, delimiter=',', fmt='%s')
 ## 
