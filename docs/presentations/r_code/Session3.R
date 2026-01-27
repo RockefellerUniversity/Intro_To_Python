c75772f8f3c0f94e2ad2acc5e5f161c6c0fd1aef
root <tc.infomatics@gmail.com>
2026-01-27 14:38:06 +0000

    Autobuild

--- docs/presentations/r_code/Session3.R
+++ docs/presentations/r_code/Session3.R
@@ -126,238 +126,92 @@ py_install(c("matplotlib", "numpy", "scipy","seaborn"), pip_options = "--quiet")
 ## plt.show()
 ## 
 
-## from scipy.optimize import curve_fit
+## ax2.hist(height_m, bins=5, color = 'royalblue', alpha = 0.5)
+## ax2.hist(height_f, bins=5, color = 'magenta', alpha = 0.5)
 ## 
-## # define the function with the independent variable first, followed by fit parameters
-## def line(x, m, b):
-##     return  m * x + b
 ## 
-## popt_m, pcov_m = curve_fit(f=line, xdata=weight_m, ydata=height_m)
-## popt_f, pcov_f = curve_fit(f=line, xdata=weight_f, ydata=height_f)
+## ax2.set_title('Height Distributions by Sex')
+## ax2.set_ylabel('Count')
+## ax2.set_xlabel('Height (cm)')
 ## 
-## print(popt_m)
-
-## x = np.linspace(start=60, stop=100, num=100)
-## print(x)
-
-## y_male = line(x, m = popt_m[0], b = popt_m[1])
-## y_female = line(x, m = popt_f[0], b = popt_f[1])
 
 ## # Generate figure and axes
 ## fig, (ax1, ax2) = plt.subplots(ncols = 2, nrows = 1, figsize=(10, 4))
 ## 
-## # Plot data
-## ax1.scatter(weight_m, height_m, c = 'royalblue', alpha = 0.5)
-## ax1.scatter(weight_f, height_f, c = 'magenta', alpha = 0.5)
+## # Plot data on ax1
+## ax1.scatter(weight_m, height_m, c = 'royalblue', alpha = 0.5, marker = 's')
+## ax1.scatter(weight_f, height_f, c = 'magenta', alpha = 0.5, marker = 'o')
 ## 
-## ax1.plot(x, y_male, c = 'royalblue')
-## ax1.plot(x, y_female, c = 'magenta')
-## 
-## ax1.set_title("Height vs Weight")
-## ax1.set_xlabel("Weight (kg)")
-## ax1.set_ylabel("Height (cm)")
-## 
-## plt.show()
-
-## # Generate figure and axes
-## fig, (ax1, ax2) = plt.subplots(ncols = 2, nrows = 1, figsize=(10, 4))
-## 
-## # Plot data
-## ax1.scatter(weight_m, height_m, c = 'royalblue', alpha = 0.5)
-## ax1.scatter(weight_f, height_f, c = 'magenta', alpha = 0.5)
-## 
-## ax1.plot(x, y_male, c = 'royalblue')
-## ax1.plot(x, y_female, c = 'magenta')
-## 
-## ax1.set_title("Height vs Weight")
-## ax1.set_xlabel("Weight (kg)")
-## ax1.set_ylabel("Height (cm)")
-## 
-## plt.show()
-
-## # Generate figure and axes
-## fig, (ax1, ax2) = plt.subplots(ncols = 2, nrows = 1, figsize=(10, 4))
-## 
-## # Plot data
-## ax1.scatter(weight_m, height_m, c = 'royalblue', alpha = 0.5, label = 'Male')
-## ax1.scatter(weight_f, height_f, c = 'magenta', alpha = 0.5, label='Female')
-## 
-## ax1.plot(x, y_male, c = 'royalblue')
-## ax1.plot(x, y_female, c = 'magenta')
-## 
-## ax1.set_title("Height vs Weight")
-## ax1.set_xlabel("Weight (kg)")
-## ax1.set_ylabel("Height (cm)")
-## 
-## ax1.legend()
-## 
-## plt.show()
-
-## # Generate figure and axes
-## fig, (ax1, ax2) = plt.subplots(ncols = 2, nrows = 1, figsize=(10, 4))
-## 
-## # Plot data
-## ax1.scatter(weight_m, height_m, c = 'royalblue', alpha = 0.5, label = 'Male')
-## ax1.scatter(weight_f, height_f, c = 'magenta', alpha = 0.5, label='Female')
-## 
-## ax1.plot(x, y_male, c = 'royalblue')
-## ax1.plot(x, y_female, c = 'magenta')
-## 
-## ax1.set_title("Height vs Weight")
-## ax1.set_xlabel("Weight (kg)")
-## ax1.set_ylabel("Height (cm)")
-## 
-## ax1.legend()
-## 
-## plt.show()
-
-## # Generate figure and axes
-## fig, (ax1, ax2) = plt.subplots(ncols = 2, nrows = 1, figsize=(10, 4))
-## 
-## # Plot data
-## ax1.scatter(weight_m, height_m, c = 'royalblue', alpha = 0.5, label = 'Male')
-## ax1.scatter(weight_f, height_f, c = 'magenta', alpha = 0.5, label='Female')
-## 
-## ax1.plot(x, y_male, c = 'royalblue')
-## ax1.plot(x, y_female, c = 'magenta')
-## 
-## ax1.set_title("Height vs Weight")
-## ax1.set_xlabel("Weight (kg)")
-## ax1.set_ylabel("Height (cm)")
-## ax1.legend()
+## # Plot data on ax2
 ## 
 ## ax2.hist(height_m, bins=5, color = 'royalblue', alpha = 0.5)
 ## ax2.hist(height_f, bins=5, color = 'magenta', alpha = 0.5)
 ## 
+## # Add labels
+## ax1.set_title("Height vs Weight")
+## ax1.set_xlabel("Weight (kg)")
+## ax1.set_ylabel("Height (cm)")
+## 
+## ax2.set_title('Height Distributions by Sex')
+## ax2.set_ylabel('Count')
+## ax2.set_xlabel('Height (cm)')
+## 
+## # Suppress automatic notebook output
 ## plt.show()
+## 
+## 
+
+## 
+## # Add legend
+## ax1.legend()
+## ax2.legend()
+## 
 
 ## # Generate figure and axes
 ## fig, (ax1, ax2) = plt.subplots(ncols = 2, nrows = 1, figsize=(10, 4))
 ## 
-## # Plot data
-## ax1.scatter(weight_m, height_m, c = 'royalblue', alpha = 0.5, label = 'Male')
-## ax1.scatter(weight_f, height_f, c = 'magenta', alpha = 0.5, label='Female')
+## # Plot data on ax1
+## ax1.scatter(weight_m, height_m, c = 'royalblue', alpha = 0.5, marker = 's')
+## ax1.scatter(weight_f, height_f, c = 'magenta', alpha = 0.5, marker = 'o')
 ## 
-## ax1.plot(x, y_male, c = 'royalblue')
-## ax1.plot(x, y_female, c = 'magenta')
-## 
-## ax1.set_title("Height vs Weight")
-## ax1.set_xlabel("Weight (kg)")
-## ax1.set_ylabel("Height (cm)")
-## ax1.legend()
+## # Plot data on ax2
 ## 
 ## ax2.hist(height_m, bins=5, color = 'royalblue', alpha = 0.5)
 ## ax2.hist(height_f, bins=5, color = 'magenta', alpha = 0.5)
 ## 
-## plt.show()
-
-## 
-## fig, (ax1, ax2) = plt.subplots(ncols = 2, nrows = 1, figsize=(10, 4))
-## ax1.scatter(weight_m, height_m, c = 'royalblue', alpha = 0.5, label = 'Male')
-## ax1.scatter(weight_f, height_f, c = 'magenta', alpha = 0.5, label='Female')
-## ax1.plot(x, y_male, c = 'royalblue')
-## ax1.plot(x, y_female, c = 'magenta')
+## # Add labels
 ## ax1.set_title("Height vs Weight")
 ## ax1.set_xlabel("Weight (kg)")
 ## ax1.set_ylabel("Height (cm)")
-## ax1.legend()
-## ax2.hist(height_m, bins=5, color = 'royalblue', alpha = 0.5, label = 'Male')
-## ax2.hist(height_f, bins=5, color = 'magenta', alpha = 0.5, label = 'Female')
 ## 
+## ax2.set_title('Height Distributions by Sex')
 ## ax2.set_ylabel('Count')
 ## ax2.set_xlabel('Height (cm)')
-## ax2.legend()
-## ax2.set_title('Height Distributions by Sex')
 ## 
-## plt.show()
-
-## 
-## fig, (ax1, ax2) = plt.subplots(ncols = 2, nrows = 1, figsize=(10, 4))
-## ax1.scatter(weight_m, height_m, c = 'royalblue', alpha = 0.5, label = 'Male')
-## ax1.scatter(weight_f, height_f, c = 'magenta', alpha = 0.5, label='Female')
-## ax1.plot(x, y_male, c = 'royalblue')
-## ax1.plot(x, y_female, c = 'magenta')
-## ax1.set_title("Height vs Weight")
-## ax1.set_xlabel("Weight (kg)")
-## ax1.set_ylabel("Height (cm)")
+## # Add legend
 ## ax1.legend()
-## ax2.hist(height_m, bins=5, color = 'royalblue', alpha = 0.5, label = 'Male')
-## ax2.hist(height_f, bins=5, color = 'magenta', alpha = 0.5, label = 'Female')
-## 
-## ax2.set_ylabel('Count')
-## ax2.set_xlabel('Height (cm)')
 ## ax2.legend()
-## ax2.set_title('Height Distributions by Sex')
 ## 
-## plt.show()
-
-## import seaborn as sns
-## 
-## fig, ax = plt.subplots(figsize = (4, 4))
-## 
-## sns.violinplot(weight_m, ax = ax)
-## 
-
-## fig, ax = plt.subplots(figsize = (4, 4))
-## 
-## sns.violinplot(weight_m, ax = ax, color = 'royalblue', alpha = 0.5, linewidth=0, label = "Male")
-## sns.violinplot(weight_f, ax = ax, color = 'magenta', alpha = 0.5, linewidth=0, label = "Female")
-## 
-## ax.set_ylabel('Weight (kg)')
-## ax.set_title("Weight Distribution by Sex")
-## ax.legend()
-## 
-## plt.show()
-
-## fig, ax = plt.subplots(figsize = (6, 5))
-## 
-## sns.violinplot(weight_m, ax = ax, color = 'royalblue', alpha = 0.5, linewidth=0, label = "Male")
-## sns.violinplot(weight_f, ax = ax, color = 'magenta', alpha = 0.5, linewidth=0, label = "Female")
-## 
-## ax.set_ylabel('Weight (kg)')
-## ax.set_title("Weight Distribution by Sex")
-## ax.legend()
-## 
-## plt.show()
-
-## fig.tight_layout()
-## fig.savefig("my_violin_plot.pdf", bbox_inches = "tight")
-
-## 
-## 
-## data = np.genfromtxt('data/gene_data.csv', unpack = True, delimiter = ",", skip_header=True)
-## 
-## print(data)
-
-## fig, ax = plt.subplots(figsize = (5,4))
-## sns.heatmap(data, linewidth = 0.5, cmap = 'viridis', annot = True)
-## ax.set_xlabel("Time")
-## ax.set_ylabel("Gene")
-## ax.set_title("Gene Progression")
+## # Suppress automatic notebook output
 ## plt.show()
 ## 
+## 
 
-## # Generate figure and axes
-## fig, ax = plt.subplots(figsize=(6, 4))
-## ratio = weight/height
-## im = ax.scatter(weight, height, c=ratio, cmap='viridis')
-## fig.colorbar(im, ax=ax)
 ## 
-## ax.set_xlabel("Weight (kg)")
-## ax.set_ylabel("Height (cm)")
-## ax.set_title("Weight vs Height")
+## x_values = np.linspace(0, 2, 100)
 ## 
-## plt.show()
+## def f(x):
+##     return x**2
+## 
+## y_values = f(x_values)
+## 
 
-## # Generate figure and axes
-## fig, ax = plt.subplots(figsize=(6, 4))
-## ratio = weight/height
-## im = ax.scatter(weight, height, c=ratio, cmap='viridis')
+## fig, ax = plt.subplots()
 ## 
-## fig.colorbar(im, ax=ax)
+## ax.plot(x_values, y_values, c = 'teal', linestyle = '--')
 ## 
-## ax.set_xlabel("Weight (kg)")
-## ax.set_ylabel("Height (cm)")
-## ax.set_title("Weight vs Height")
+## ax.set_title("Quadratic Function $y = x^2$")
+## ax.set_xlabel("x")
+## ax.set_ylabel("y")
 ## 
 ## plt.show()
